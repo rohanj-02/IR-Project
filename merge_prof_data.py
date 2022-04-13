b367bca49c50a9caf548bbe0c8c69bf4e2ba0225
Rohan Jain <rohan19095@iiitd.ac.in>
2022-04-13 21:42:56 +0530

    merge prof data

--- merge_prof_data.py
+++ merge_prof_data.py
@@ -15,24 +15,34 @@ for a_id in author_ids:
         print(f"No documents for {a_id}")
         continue
     final = {}
-    with open(f"{OUT_DIR}/{f}", "r") as infile:
+    with open(f"{OUT_DIR}/{filtered[0]}", "r") as infile:
         data = json.load(infile)
-    final = data
-    for doc in filtered:
+        final = data
+    # print(a_id)
+    # print(final.keys())
+    if "articles" not in final.keys():
+        final["articles"] = []
+    if "co_authors" not in final.keys():
+        final["co_authors"] = []
+    for doc in filtered[1:]:
         with open(f"{OUT_DIR}/{doc}", "r") as infile:
             data = json.load(infile)
-        final["articles"] += data["articles"]
-        final["co-authors"] += data["co-authors"]
+            if "articles" in data.keys():
+                final["articles"] += data["articles"]
+            if "co_authors" in data.keys():
+                final["co_authors"] += data["co_authors"]
 
-    # remove duplicate co-authors
-    ids = [x["author_id"] for x in final["co-authors"]]
+    # remove duplicate co_authors
+    ids = [x["author_id"] for x in final["co_authors"]]
     ids = list(set(ids))
     final_co_authors = []
-    for idx in range(len(final["co-authors"])):
-        if final["co-authors"][idx]["author_id"] in ids:
-            final_co_authors.append(final["co-authors"][idx])
-            ids.remove(final["co-authors"][idx]["author_id"])
-    final["co-authors"] = final_co_authors
+    for idx in range(len(final["co_authors"])):
+        if final["co_authors"][idx]["author_id"] in ids:
+            final_co_authors.append(final["co_authors"][idx])
+            ids.remove(final["co_authors"][idx]["author_id"])
+    final["co_authors"] = final_co_authors
+    final["search_metadata"]["total_articles"] = len(final["articles"])
+    final["search_metadata"]["total_co_authors"] = len(final["co_authors"])
 
     with open(f"{OUT_DIR}/final/{a_id}.json", "w") as outfile:
         json.dump(final, outfile, indent=4)
