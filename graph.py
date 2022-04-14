f94f77da32132986117a6c121a8390bb63476be1
gitansh19241 <gitansh19241@iiitd.ac.in>
2022-04-14 13:47:09 +0530

    co authorship graph added

--- graph.py
+++ graph.py
@@ -9,8 +9,11 @@ class Author:
         self.articles = articles
         self.authorCategory = authorCategory
         self.penNames = penNames
+
     def printAuthor(self):
-        print('Name =',self.name,'Type =',self.authorType, 'Category =',self.authorCategory, 'NameList =',self.penNames)
+        print('Name =', self.name, 'Type =', self.authorType, 'Category =', self.authorCategory, 'NameList =',
+              self.penNames)
+
 
 class Graph:
     def __init__(self, graph={}):
@@ -49,6 +52,7 @@ facultyData = os.listdir(filepath)
 studentData = os.listdir(filepath2)
 facultyList = []
 studentList = []
+aList = []
 for file in facultyData:
     with open(filepath + file, "r") as f:
         author = json.load(f)
@@ -62,69 +66,125 @@ for file in facultyData:
     nlist = [name.lower()]
     name = name.lower()
     temp = name.split(' ')
+    if name=='raghava mutharaju':
+        nlist.append('vr mutharaju')
+    if name=='vivek bohara':
+        nlist.append('va bohara')
+    if name=='sanat biswas':
+        nlist.append('sk biswas')
+    if name == 'bijendra jain':
+        nlist.append('bn jain')
+    if name == 'mukesh mohania':
+        nlist.append('mk mohania')
     if name == "gajendra ps raghava":
-        nlist.append('GPS Raghava')
-        nlist.append('G Raghava')
-        nlist.append('GP Raghava')
-        nlist.append('Gs Raghava')
+        nlist.append('gps raghava')
+        nlist.append('g raghava')
+        nlist.append('gp Raghava')
+        nlist.append('gs Raghava')
     elif len(temp) == 2:
-        nlist.append(temp[0] + ' '+temp[1][0])
-        nlist.append(temp[0][0] + ' '+temp[1])
+        nlist.append(temp[0] + ' ' + temp[1][0])
+        nlist.append(temp[0][0] + ' ' + temp[1])
     elif len(temp) == 3:
         nlist.append(temp[0][0] + temp[1][0] + ' ' + temp[2])
-        nlist.append(temp[0][0] + ' '+temp[1][0] + ' ' + temp[2])
+        nlist.append(temp[0][0] + ' ' + temp[1][0] + ' ' + temp[2])
         nlist.append(temp[0][0] + ' ' + temp[2])
         nlist.append(temp[0] + ' ' + temp[2])
-        nlist.append(temp[0][0] + ' '+temp[1] + ' ' + temp[2])
-        nlist.append(temp[0] + ' '+temp[1][0] + ' ' + temp[2])
-        nlist.append(temp[0] + ' '+temp[1][0] + ' ' + temp[2][0])
-        nlist.append(temp[0] + ' '+temp[1] + ' ' + temp[2][0])
-        nlist.append(temp[0] +' ' +temp[1][0] + temp[2][0])
-        nlist.append(temp[0][0] +' '+ temp[1] + ' ' + temp[2][0])
+        nlist.append(temp[0][0] + ' ' + temp[1] + ' ' + temp[2])
+        nlist.append(temp[0] + ' ' + temp[1][0] + ' ' + temp[2])
+        nlist.append(temp[0] + ' ' + temp[1][0] + ' ' + temp[2][0])
+        nlist.append(temp[0] + ' ' + temp[1] + ' ' + temp[2][0])
+        nlist.append(temp[0] + ' ' + temp[1][0] + temp[2][0])
+        nlist.append(temp[0][0] + ' ' + temp[1] + ' ' + temp[2][0])
     elif len(temp) == 4:
         nlist.append(temp[0][0] + temp[1][0] + temp[2][0] + ' ' + temp[3])
     else:
         print('Not Processed')
-    facultyList.append(Author(name.lower(),'faculty','iiitd',nlist, author['articles']))
-
+    aList.append(Author(name.lower(), 'faculty', 'iiitd', nlist, author['articles']))
 
 for file in studentData:
     with open(filepath2 + file, "r") as f:
         author = json.load(f)
     name = author['author']['name']
-    nlist=[name.lower()]
-    name=name.lower()
-    temp=name.split(' ')
-    if len(temp)==1:
+    nlist = [name.lower()]
+    name = name.lower()
+    temp = name.split(' ')
+    if name == 'kuldeep yadav':
+        nlist.append('ks yadav')
+    if len(temp) == 1:
         pass
-    elif len(temp)==2:
-        nlist.append(temp[0] + ' '+temp[1][0])
-        nlist.append(temp[0][0] + ' '+temp[1])
-    elif len(temp)==3:
+    elif len(temp) == 2:
+        nlist.append(temp[0] + ' ' + temp[1][0])
+        nlist.append(temp[0][0] + ' ' + temp[1])
+    elif len(temp) == 3:
         nlist.append(temp[0][0] + temp[1][0] + ' ' + temp[2])
-        nlist.append(temp[0][0] + ' '+temp[1][0] + ' ' + temp[2])
+        nlist.append(temp[0][0] + ' ' + temp[1][0] + ' ' + temp[2])
         nlist.append(temp[0][0] + ' ' + temp[2])
         nlist.append(temp[0] + ' ' + temp[2])
-        nlist.append(temp[0][0] + ' '+temp[1] + ' ' + temp[2])
-        nlist.append(temp[0] + ' '+temp[1][0] + ' ' + temp[2])
-        nlist.append(temp[0] + ' '+temp[1][0] + ' ' + temp[2][0])
-        nlist.append(temp[0] + ' '+temp[1] + ' ' + temp[2][0])
-        nlist.append(temp[0] +' ' +temp[1][0] + temp[2][0])
-        nlist.append(temp[0][0] +' '+ temp[1] + ' ' + temp[2][0])
-    elif len(temp)==4:
-        nlist.append(temp[0][0]+temp[1][0]+temp[2][0]+' '+temp[3])
+        nlist.append(temp[0][0] + ' ' + temp[1] + ' ' + temp[2])
+        nlist.append(temp[0] + ' ' + temp[1][0] + ' ' + temp[2])
+        nlist.append(temp[0] + ' ' + temp[1][0] + ' ' + temp[2][0])
+        nlist.append(temp[0] + ' ' + temp[1] + ' ' + temp[2][0])
+        nlist.append(temp[0] + ' ' + temp[1][0] + temp[2][0])
+        nlist.append(temp[0][0] + ' ' + temp[1] + ' ' + temp[2][0])
+    elif len(temp) == 4:
+        nlist.append(temp[0][0] + temp[1][0] + temp[2][0] + ' ' + temp[3])
     else:
         print('Not Processed')
-    studentList.append(Author(name.lower(),'student', 'iiitd',nlist,author['articles']))
+    aList.append(Author(name.lower(), 'student', 'iiitd', nlist, author['articles']))
 
-G.addEdge(studentList[0],studentList[3])
-G.addEdge(studentList[3],studentList[5])
-G.addEdge(studentList[3],studentList[5])
-print(G.graph)
-# for s in facultyList:
-#     s.printAuthor()
+l = []
+# for i in aList:
+#     i.printAuthor()
 
-# for file in facultyData:
-#     with open(filepath + file, "r") as f:
-#         author = json.load(f)
-#     facultyList.append(author['author']['name'])
+with open('authorData.josn', 'r') as f:
+    data = json.load(f)
+objList = []
+newList = []
+count = 0
+rlist=[]
+for paper in data:
+    data[paper] = [i.lower() for i in data[paper]]
+    temp = []
+    c = 0
+    for i in data[paper]:
+        f = 0
+        for stat in aList:
+            if i in stat.penNames:
+                c += 1
+                if stat in temp:
+                    continue
+                temp.append(stat)
+                count += 1
+                f = 1
+                break
+        if f == 0:
+            newList.append(Author(i, 'unknown', 'non_iiitd', [i]))
+            temp.append(newList[len(newList) - 1])
+    if c == 0:
+        rlist+=data[paper]
+        # print(paper, data[paper])
+    objList.append(temp)
+# rlist = sorted(rlist, key = rlist.count,reverse = True)
+# print(result)
+print(len(objList), count)
+# rdict={}
+# for i in rlist:
+#     if i in rdict.keys():
+#         rdict[i]+=1
+#     else:
+#         rdict[i]=1
+# print(rdict)
+ctr = 0
+for combo in objList:
+    for i in range(len(combo)-1):
+        for j in range(i+1,len(combo)):
+            G.addEdge(combo[i],combo[j])
+
+# ddict={1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0,19:0,20:0,}
+# for a in G.graph:
+#     for b in G.graph[a]:
+#         if G.graph[a][b]>20:
+#             print(a.name,b.name)
+#         else:
+#             ddict[G.graph[a][b]]+=1
+# print(ddict)
