c9368e180691c93931b7c0213604e07f2da7977b
WizardOfMOz <aaloke19004@iiitd.ac.in>
2022-04-27 16:24:35 +0530

    search engine -graph based

--- SearchBox.py
+++ SearchBox.py
@@ -1,6 +1,30 @@
 from flask import Flask, render_template, request
 from flask.helpers import flash
 import pickle
+import numpy as np
+import pickle
+import networkx as nx
+
+with open('Models/graph.pkl','rb') as f:
+    G=pickle.load(f)    
+
+iiitd_profs=[i for i in list(G.nodes) if G.nodes[i]['authorCategory']=='iiitd']
+G_=G.subgraph(iiitd_profs)
+
+
+
+def get_domain_networks(query,homogeneous=True):    
+    g=G_ if homogeneous==True else G
+    domain_list=nx.get_node_attributes(g,'domains')
+    domain_authors=[author for author in domain_list if query in domain_list[author].keys()]
+    domain_cliques=list(nx.algorithms.find_cliques(g.subgraph(domain_authors)))
+    domain_scores=[np.mean([domain_list[author][query] for author in clique]) for clique in domain_cliques]
+    clique_scores=list(zip(domain_cliques,domain_scores))
+    clique_scores.sort(reverse=True,key=lambda x:x[1])
+    search_results=[clique[0] for clique in clique_scores[:min(10,len(clique_scores))] ]
+    return search_results
+
+
 
 app = Flask(__name__)
 with open("AllDomains.pkl","rb") as file:
@@ -14,8 +38,11 @@ def home():
     if request.method=='POST':
         domain = request.form['search-input']
         selected = domain
-        print(selected)
+        print(get_domain_networks(selected))
     return render_template('index.html',domains=domains,selected=selected)
+
+
+
 
 
 if __name__ == '__main__':
